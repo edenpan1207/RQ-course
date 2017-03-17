2d4f306c467f8aced123411087264406e4326551
edenpan1207 <xadam69@gmail.com>
2017-03-17 16:15:46 +0800

    add new pages

--- js/course.js
+++ js/course.js
@@ -1,10 +1,19 @@
-$(document).ready(function(){
-      $(".center").slick({
+$(document).ready(function() {
+    $(".center").slick({
         dots: true,
         infinite: true,
         centerMode: true,
         variableWidth: true,
         slidesToShow: 3,
         slidesToScroll: 3
-      });
     });
+
+    var the_terms = $("#checkbox-submit");
+    the_terms.click(function() {
+        if ($(this).is(":checked")) {
+            $("#submit").removeAttr("disabled").css('opacity','1');
+        } else {
+            $("#submit").attr("disabled", "disabled").css('opacity','0.5');
+        }
+    });
+});
