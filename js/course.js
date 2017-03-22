65e65522340f1b60ca3a8e3b8ff45299480c9538
edenpan1207 <xadam69@gmail.com>
2017-03-22 11:43:20 +0800

    fix search box

--- js/course.js
+++ js/course.js
@@ -11,51 +11,49 @@ $(document).ready(function() {
     var the_terms = $("#checkbox-submit");
     the_terms.click(function() {
         if ($(this).is(":checked")) {
-            $("#submit").removeAttr("disabled").css('opacity','1');
+            $("#submit").removeAttr("disabled").css('opacity', '1');
         } else {
-            $("#submit").attr("disabled", "disabled").css('opacity','0.5');
+            $("#submit").attr("disabled", "disabled").css('opacity', '0.5');
         }
     });
 
-    $('#course').on('change', function(){
-    var val = $(this).val(); // 將下拉式的值，存起來
-    $('[data-coach]').show(); // 先將列表全部顯示
-    $.each($('[data-coach]'), function(){  // 處理全部有 data-course 的物件
-      console.log($(this));  // 列出來有哪些
-      if ($(this).data('coach') !== val) {  // 如果和下拉式的選單不同
-          $(this).parents().parents().parents().hide();  // 就隱藏
-      } else {
-         $(this).parents().show();
-      }
+    $('#coach').on('change', function() {
+        var val = $(this).val();
+        $('[data-coach]').show();
+        $.each($('[data-coach]'), function() {
+          console.log($(this));
+            if ($(this).data('coach') !== val) {
+                $(this).parent().parent().hide();
+            } else {
+                $(this).parent().parents().show();
+            }
+        });
     });
-  });
-  
-  
-  $('#coach').on('change', function(){
-    var val = $(this).val(); // 將下拉式的值，存起來
-    $('[data-coach]').show(); // 先將列表全部顯示
-    $.each($('[data-coach]'), function(){  // 處理全部有 data-course 的物件
-      console.log($(this));  // 列出來有哪些
-      if ($(this).data('coach') !== val) {  // 如果和下拉式的選單不同
-          $(this).parent().parent().hide();  // 就隱藏
-      } else {
-         $(this).parents().show();
-      }
+
+    $('#location').on('change', function() {
+        var val = $(this).val(); 
+        $('[data-location]').show();
+        $.each($('[data-location]'), function() { 
+            
+            if ($(this).data('location') !== val) { 
+                $(this).parent().parent().parent().hide(); 
+            } else {
+                $(this).parent().parent().parents().show();
+            }
+        });
     });
-  });
-  
-  
-  $('#time').on('change', function(){
-    var val = $(this).val(); // 將下拉式的值，存起來
-    $('[data-time]').show(); // 先將列表全部顯示
-    $.each($('[data-time]'), function(){  // 處理全部有 data-course 的物件
-      console.log($(this));  // 列出來有哪些
-      if ($(this).data('time') !== val) {  // 如果和下拉式的選單不同
-          $(this).parent().hide();  // 就隱藏
-      } else {
-         $(this).parents().show();
-      }
+
+    $('#time').on('change', function() {
+        var val = $(this).val(); 
+        $('[data-time]').show();
+        $.each($('[data-time]'), function() { 
+          console.log($(this));
+            if ($(this).data('time') !== val) {
+                $(this).parent().parent().parent().hide(); 
+            } else {
+                $(this).parent().parent().parents().show();
+            }
+        });
     });
-  });
-  
+
 });
