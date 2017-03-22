ca051efe67ee7f0b0492ae5a58861b6e437fa9d2
edenpan1207 <xadam69@gmail.com>
2017-03-22 11:24:01 +0800

    fixed process content

--- js/course.js
+++ js/course.js
@@ -16,4 +16,46 @@ $(document).ready(function() {
             $("#submit").attr("disabled", "disabled").css('opacity','0.5');
         }
     });
+
+    $('#course').on('change', function(){
+    var val = $(this).val(); // 將下拉式的值，存起來
+    $('[data-coach]').show(); // 先將列表全部顯示
+    $.each($('[data-coach]'), function(){  // 處理全部有 data-course 的物件
+      console.log($(this));  // 列出來有哪些
+      if ($(this).data('coach') !== val) {  // 如果和下拉式的選單不同
+          $(this).parents().parents().parents().hide();  // 就隱藏
+      } else {
+         $(this).parents().show();
+      }
+    });
+  });
+  
+  
+  $('#coach').on('change', function(){
+    var val = $(this).val(); // 將下拉式的值，存起來
+    $('[data-coach]').show(); // 先將列表全部顯示
+    $.each($('[data-coach]'), function(){  // 處理全部有 data-course 的物件
+      console.log($(this));  // 列出來有哪些
+      if ($(this).data('coach') !== val) {  // 如果和下拉式的選單不同
+          $(this).parent().parent().hide();  // 就隱藏
+      } else {
+         $(this).parents().show();
+      }
+    });
+  });
+  
+  
+  $('#time').on('change', function(){
+    var val = $(this).val(); // 將下拉式的值，存起來
+    $('[data-time]').show(); // 先將列表全部顯示
+    $.each($('[data-time]'), function(){  // 處理全部有 data-course 的物件
+      console.log($(this));  // 列出來有哪些
+      if ($(this).data('time') !== val) {  // 如果和下拉式的選單不同
+          $(this).parent().hide();  // 就隱藏
+      } else {
+         $(this).parents().show();
+      }
+    });
+  });
+  
 });
